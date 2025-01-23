f74be572433855e801d5e9bb9b4cbb02435f9162
zhongyongsheng <itvincent@gmail.com>
2025-01-23 22:05:22 +0800

    feat(prettier): fix prettier; add xlayout; change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,12 @@ const eslintConfig = [
     },
   },
   eslintConfigPrettier,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    }
+  }
 ]
 
 export default eslintConfig
