7d024e3a90d5a27f9cb1e442558e9525bb2fe0ce
zhongyongsheng <itvincent@gmail.com>
2025-07-30 21:37:27 +0800

    feat(layout): read henv

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -73,8 +73,8 @@ export default async function RootLayout({
               baseUrl: henv('X_HTTP_BASE') || '',
               headers: { 'Accept-Language': locale },
               requestOptionsConfig: {
-                logging: true,
-                slowThreshold: 500,
+                logging: henv('X_DEBUG_HTTP_LOG') !== '0', //defalt enable log, 1: enable debug log, 0: disable debug log
+                slowThreshold: parseInt(henv('X_HTTP_LOG_SLOW_TIME') || '1000'), //defalt 1000ms slow log threshold
               },
             }}
           >
